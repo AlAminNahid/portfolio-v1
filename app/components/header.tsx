f6c666e688803bb962f339c7fee9292da4f6fd1c
AlAminNahid <robin.nahid123@gmail.com>
2026-03-13 19:19:10 +0600

    done the nav and the home section

---
+++ app/components/header.tsx
@@ -0,0 +1,58 @@
+import Image from "next/image";
+import profileImage from "@/public/assets/profileIMG.jpg";
+import handIcon from "@/public/assets/hand-icon.png";
+import rightArrowWhite from "@/public/assets/right-arrow-white.png";
+import downloadIcon from "@/public/assets/download-icon.png";
+
+export default function Header() {
+  return (
+    <>
+      <div className="w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4">
+        <div>
+          <Image
+            src={profileImage}
+            alt="Profile-Image"
+            className="rounded-full w-32"
+          />
+        </div>
+
+        <h3 className="flex items-end gap-2 text-xl md:text-2xl mb-3 font-ovo">
+          Hi! I'm Al Amin Hossain Nahid
+          <Image src={handIcon} alt="Hand-Icon" className="w-6" />
+        </h3>
+
+        <h1 className="text-3xl sm:text-6xl lg:text-[66px] font-ovo">
+          Full-Stack Web & Mobile Developer
+        </h1>
+
+        <p className="max-w-2xl mx-auto font-ovo">
+          Passionate about creating seamless web and mobile experiences from
+          front-end design to back-end systems.
+        </p>
+
+        <div className="flex flex-col sm:flex-row items-center gap-4 mt-4">
+          <a
+            href="#contact"
+            className="px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2"
+          >
+            contact me
+            <Image
+              src={rightArrowWhite}
+              alt="Right-Arrow-White"
+              className="w-4"
+            />
+          </a>
+
+          <a
+            href="/Nahid_s_Resume.pdf"
+            download
+            className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2"
+          >
+            my resume
+            <Image src={downloadIcon} alt="Download-Icon" className="w-4" />
+          </a>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ app/components/navBar.tsx
@@ -0,0 +1,122 @@
+"use client";
+
+import Image from "next/image";
+import logo from "@/public/assets/logo.png";
+import arrowIcon from "@/public/assets/arrow-icon.png";
+import headerBG from "@/public/assets/header-bg-color.png";
+import moonIcon from "@/public/assets/moon_icon.png";
+import menuBlackIcon from "@/public/assets/menu-black.png";
+import closeIcon from "@/public/assets/close-black.png";
+import { useRef } from "react";
+
+export default function NavBar() {
+  const sideMenuRef = useRef<HTMLUListElement | null>(null);
+
+  const openMenu = () => {
+    if (sideMenuRef.current) {
+      sideMenuRef.current.style.transform = "translateX(-16rem)";
+    }
+  };
+  const closeMenu = () => {
+    if (sideMenuRef.current) {
+      sideMenuRef.current.style.transform = "translateX(16rem)";
+    }
+  };
+  return (
+    <>
+      <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]">
+        <Image src={headerBG} alt="header-BG" className="w-full" />
+      </div>
+
+      {/* Big Screen View */}
+
+      <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50">
+        <a href="#top">
+          <Image src={logo} className="w-28 cursor-pointer mr-14" alt="Logo" />
+        </a>
+        <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50">
+          <li>
+            <a className="font-ovo" href="#top">
+              Home
+            </a>
+          </li>
+          <li>
+            <a className="font-ovo" href="#about">
+              About Me
+            </a>
+          </li>
+          <li>
+            <a className="font-ovo" href="#services">
+              Services
+            </a>
+          </li>
+          <li>
+            <a className="font-ovo" href="#work">
+              My Work
+            </a>
+          </li>
+          <li>
+            <a className="font-ovo" href="#contact">
+              Contact Me
+            </a>
+          </li>
+        </ul>
+        <div className="flex items-center gap-4">
+          <button>
+            <Image src={moonIcon} alt="Moon-Icon" className="w-6" />
+          </button>
+          <a
+            href="#contact"
+            className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-ovo"
+          >
+            Contact <Image src={arrowIcon} className="w-3" alt="Arrow Icon" />
+          </a>
+          <button className="block md:hidden ml-3" onClick={openMenu}>
+            <Image src={menuBlackIcon} alt="Moon-Icon" className="w-6" />
+          </button>
+        </div>
+
+        {/* Mobile Menu */}
+
+        <ul
+          ref={sideMenuRef}
+          className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500"
+        >
+          <div className="absolute right-6 top-6" onClick={closeMenu}>
+            <Image
+              src={closeIcon}
+              alt="Close-Icon"
+              className="w-5 cursor-pointer"
+            />
+          </div>
+
+          <li>
+            <a className="font-ovo" onClick={closeMenu} href="#top">
+              Home
+            </a>
+          </li>
+          <li>
+            <a className="font-ovo" onClick={closeMenu} href="#about">
+              About Me
+            </a>
+          </li>
+          <li>
+            <a className="font-ovo" onClick={closeMenu} href="#services">
+              Services
+            </a>
+          </li>
+          <li>
+            <a className="font-ovo" onClick={closeMenu} href="#work">
+              My Work
+            </a>
+          </li>
+          <li>
+            <a className="font-ovo" onClick={closeMenu} href="#contact">
+              Contact Me
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
